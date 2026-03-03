9b89173a00b24c5ff903051f1ad09bb297932efd
Torerro <torerro1996@mail.ru>
2026-03-03 13:31:52 +0300

    обновил upload middleware

--- backend/src/middleware/upload.ts
+++ backend/src/middleware/upload.ts
@@ -2,7 +2,6 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-// TODO обезопасить например от этого '../../../etc/passwd'
 const storage = multer.diskStorage({
   destination: (_req, _file, cb) => {
     const dir = "./uploads/animals";
@@ -11,28 +10,24 @@ const storage = multer.diskStorage({
   },
   filename: (_req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
+    const ext = path.extname(file.originalname).toLowerCase();
+    cb(null, uniqueSuffix + ext);
   },
 });
 
 export const upload = multer({
   storage,
-  // Ограничим размер файла, например, до 5МБ
   limits: {
     fileSize: 5 * 1024 * 1024,
+    files: 5,
   },
-  // Фильтр по типам файлов
   fileFilter: (_req, file, cb) => {
     const allowedTypes = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
 
     if (allowedTypes.includes(file.mimetype)) {
       cb(null, true);
     } else {
-      cb(
-        new Error(
-          "Недопустимый формат файла. Разрешены только jpeg, jpg, png и webp."
-        )
-      );
+      cb(new Error("Недопустимый формат файла. Разрешены только изображения."));
     }
   },
 });
